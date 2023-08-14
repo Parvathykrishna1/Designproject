78284b23ab757f63cdcbf1f61bcf4eafb6ab9ac2
parvathy <parvathyk285@gmail.com>
2023-08-14 13:49:18 +0530

    Initial commit

---
+++ design/app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from app1.models import Place
+
+from app1.models import Team
+
+
+# Create your views here.
+def home(request):
+    p=Place.objects.all()
+    t=Team.objects.all()
+    return render(request, 'home.html', {'p':p,'t':t})
+
+def contact(request):
+    return render(request,'contact.html')
+def destinations(request):
+    return render(request,'destinations.html')
+def elements(request):
+    return render(request,'elements.html')
+def news(request):
+    return render(request,'news.html')
